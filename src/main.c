7c90b4d061982e936c59f9a94c4a8d00c1763a67
saulgold <saulgold@gmail.com>
2016-04-23 17:14:07 +0100

    kind of records but not really

--- src/main.c
+++ src/main.c
@@ -25,12 +25,13 @@
 #include <dsp.h>
 #include <ADCChannelDrv.h>
 #include <OCPWMDrv.h>
+#include <libpic30.h>
 #include "..\inc\AT25F4096Drv.h"
 #include "..\inc\G711.h"
 #include "..\inc\frequency_processing.h"
 #include "..\inc\modulate.h"
 #include "..\inc\transform.h"
-#define FRAME_SIZE 			256
+#define FRAME_SIZE 			128
 #define WRITE_START_ADDRESS	0x20000		/* Flash memory address for user				*/
 
 
@@ -70,13 +71,18 @@ long userPlaybackAddress;		/* This one tracks user playback		*/
 int record;							/* If set means recording			*/
 int playback;							/* If set means playback is in progress	*/
 int erasedBeforeRecord;				/* If set means that erase before record	*/
-									/* is complete	*/ 
+int fileSize=0;									/* is complete	*/ 
 long address;							/* Used for erasing the flash			*/
-
-
+int storage [10][128];
+int i1 __attribute__((space(dma)));
+int j __attribute__((space(dma))); 
+int k __attribute__((space(dma)));
 int main(void)
 {
-	
+	j=0;
+	k=0;
+		float clock_frequency, cycle_time, delay_time;
+	unsigned long delay_cycles;
 	/* Configure Oscillator to operate the device at 40MHz.
 	 * Fosc= Fin*M/(N1*N2), Fcy=Fosc/2
 	 * Fosc= 7.37M*40/(2*2)=80Mhz for 7.37M input clock */
@@ -84,7 +90,12 @@ int main(void)
 	PLLFBD=41;				/* M=39	*/
 	CLKDIVbits.PLLPOST=0;		/* N1=2	*/
 	CLKDIVbits.PLLPRE=0;		/* N2=2	*/
-	OSCTUN=0;			
+	OSCTUN=0;	
+			
+		clock_frequency = 40e6;
+	cycle_time = 1 / clock_frequency;
+	delay_time = 0.02;
+	delay_cycles = delay_time / cycle_time;
 	
 	__builtin_write_OSCCONH(0x01);		/*	Initiate Clock Switch to FRC with PLL*/
 	__builtin_write_OSCCONL(0x01);
@@ -117,7 +128,8 @@ int main(void)
 	/*Start Audio input and output function*/
 	ADCChannelStart	(pADCChannelHandle);
 	OCPWMStart		(pOCPWMHandle);	
-
+	
+	
 	/*start processing loop*/	
 	while(1)
 	{   
@@ -125,53 +137,18 @@ int main(void)
 			while(ADCChannelIsBusy(pADCChannelHandle));
 			ADCChannelRead	(pADCChannelHandle,samples,FRAME_SIZE);
 
-			
+			i1=0;
 		if( record==1){
+			k=0;
+
 			
-			if(erasedBeforeRecord == 0){
-				/* Stop the Audio input and output since this is a blocking
-				 * operation. Also rewind record and playback pointers to
-				 * start of the user flash area.*/
-					 
-				ADCChannelStop(pADCChannelHandle);
-				OCPWMStop	(pOCPWMHandle);
-				currentWriteAddress = WRITE_START_ADDRESS;
-				userPlaybackAddress = WRITE_START_ADDRESS;
-				RED_LED = SASK_LED_ON;
-				YELLOW_LED = SASK_LED_OFF;
-				/* Erase the user area of the flash. The intro message is not erased	*/
-				for(address = WRITE_START_ADDRESS; address < AT25F4096DRV_LAST_ADDRESS; address += 0x10000){
-					/* Erase each sector. Each sector is 0xFFFF long	*/
-					AT25F4096IoCtl(pFlashMemoryHandle,AT25F4096DRV_WRITE_ENABLE,0);	
-					AT25F4096IoCtl(pFlashMemoryHandle,AT25F4096DRV_SECTOR_ERASE,(void *)&address);	
-					while(AT25F4096IsBusy(pFlashMemoryHandle));
-					}
-				RED_LED = 1;
-				/* Since erase is complete, the next time the loop is executed
-				 * dont erase the flash. Start the audio input and output	*/
-				erasedBeforeRecord = 1;
-				ADCChannelStart(pADCChannelHandle);
-				OCPWMStart		(pOCPWMHandle);	
+				//G711Lin2Ulaw(samples,encodedSamples,FRAME_SIZE);
+				for(i1=0;i1<FRAME_SIZE;i1=i1+1){
+					storage[j][i1] = samples[i1];
 					
-	    	}	
-			else{
-				/* Record the encoded audio frame. Yellow LED turns on when
-				 * when recording is being performed	*/
-				YELLOW_LED=0;
-				G711Lin2Ulaw(samples,encodedSamples,FRAME_SIZE);
-				
-				while(AT25F4096IsBusy(pFlashMemoryHandle));
-				AT25F4096IoCtl(pFlashMemoryHandle,AT25F4096DRV_WRITE_ENABLE,0);
-				AT25F4096Write(pFlashMemoryHandle,currentWriteAddress,encodedSamples,FRAME_SIZE);
-				currentWriteAddress += FRAME_SIZE;
-				
-				if(currentWriteAddress >= AT25F4096DRV_LAST_ADDRESS){
-					YELLOW_LED = SASK_LED_OFF;
-					erasedBeforeRecord = 0;
-					record = 0;
-				
 				}
-			}		
+				j++;			
+							
 		}
 		/* If playback is enabled, then start playing back samples from the
 		 * user area. Playback only till the last record address and then 
@@ -182,68 +159,94 @@ int main(void)
 
 			
 		if ( playback==1){
+			j=0;
 				
-			GREEN_LED = SASK_LED_ON;
-			erasedBeforeRecord = 0;		
-			while(AT25F4096IsBusy(pFlashMemoryHandle));				
-			AT25F4096Read(pFlashMemoryHandle,userPlaybackAddress,encodedSamples,FRAME_SIZE);
-			while(AT25F4096IsBusy(pFlashMemoryHandle));
-			userPlaybackAddress += FRAME_SIZE;
-			if(userPlaybackAddress >= currentWriteAddress){
-				userPlaybackAddress = WRITE_START_ADDRESS;
-			}
+
+			for(i1=0;i1<FRAME_SIZE;i1++){
+					samples[i1] = storage[k][i1];
+				
+				}
+				
+				k++;
 		}
-			
+				while(OCPWMIsBusy(pOCPWMHandle));	
+			OCPWMWrite (pOCPWMHandle,samples,FRAME_SIZE);
 			/* Decode the samples	*/
-			G711Ulaw2Lin (encodedSamples,decodedSamples, FRAME_SIZE);
-	
+			//G711Ulaw2Lin (encodedSamples,decodedSamples, FRAME_SIZE);
+	    //	__delay32( delay_cycles );
 			/* Wait till the OC is available for a new  frame	*/
-			while(OCPWMIsBusy(pOCPWMHandle));	
 		
-			/* Write the frame to the output	*/
-			OCPWMWrite (pOCPWMHandle,decodedSamples,FRAME_SIZE);
 			
 				
 			/* The CheckSwitch functions are defined in sask.c	*/
 
-			if((CheckSwitchS1()) == 1)
+			if(SWITCH_S1== 0)
 			{
-				/* Toggle the record function and Yellow led.
-				 * Rewind the intro message playback pointer. 
-				 * And if recording, disable playback.*/
-				 
+					 
 				
-				record = 1;				
-				currentReadAddress = 0;	
-				erasedBeforeRecord = 0;
-				if(record == 1)
-				{
-					playback = 0;
-					GREEN_LED = SASK_LED_OFF;
-				}
-				else
-				{
-					YELLOW_LED = SASK_LED_OFF;
-				}
+				record = 1;		
+				playback=0;
+						
+				GREEN_LED =0;
+				YELLOW_LED=1;
 			}
 			
-			if((CheckSwitchS2()) == 1)
+			if(SWITCH_S2 == 0)
 			{
 				/* Toggle the record function and AMBER led.
 				 * Rewind the intro message playback pointer. 
 				 * And if recording, disable playback.*/
-				 
-				GREEN_LED =SASK_LED_OFF;
-				playback =1;
-				currentReadAddress = 0;	
-				userPlaybackAddress = WRITE_START_ADDRESS;	
-				if(playback == 1)
-				{
-					record = 0;
-					YELLOW_LED = SASK_LED_OFF;
-				}
+				playback=1; 
+				GREEN_LED =1;
+				YELLOW_LED=0;
+				record = 0;
 			}
-			
+			if(SWITCH_S1==1 && SWITCH_S2==1){
+				playback =0;
+				record=0;
+				GREEN_LED =1;
+				YELLOW_LED=1;				
+			}
 	
 }
+}
+
+
+void WriteToSerialFlash(AT25F4096Handle * pHandle, unsigned long address, char * source, int length)
+{
+	/* This function will write length number of bytes from 
+	 * source to serial flash memory location address*/
+	 
+	 int acceptedBytes = 0;
+	
+	while(length != 0 )
+	{
+		while(AT25F4096IsBusy(pHandle));
+		AT25F4096IoCtl(pHandle,AT25F4096DRV_WRITE_ENABLE,0);	
+		acceptedBytes = AT25F4096Write(pHandle,address,(source+acceptedBytes),length);
+		length -= acceptedBytes;
+		address += acceptedBytes;
+		while(AT25F4096IsBusy(pHandle));
+	}
+}
+
+int VerifyWriteToSerialFlash(AT25F4096Handle * pHandle, unsigned long address, char * verifyWithData, int length)	
+{
+	/* This function will compare the serial flash data stored at
+	 * address and verify it with data in verifyWithData. It will 
+	 * return a 1 if comparision passes, 0 otherwise */
+	 
+	int 		verifyIndex;
+	char	verifyFlashRead;
+	
+	for(verifyIndex = 0; verifyIndex < length;verifyIndex++,address++)
+	{
+		AT25F4096Read(pHandle,address,&verifyFlashRead,1);
+		while(AT25F4096IsBusy(pHandle));
+		if(verifyFlashRead !=verifyWithData[verifyIndex])
+		{
+			return(0);
+		}
+	}
+	return(1);
 }
